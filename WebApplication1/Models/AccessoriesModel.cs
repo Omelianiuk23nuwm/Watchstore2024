663d3d295a053c1d734d4b0da599a4bd5da8de15
Omelianiuk23nuwm <omelianiuk_ak23@nuwm.edu.ua>
2024-06-20 18:59:00 +0300

    del //
    Remove

--- WebApplication1/Models/AccessoriesModel.cs
+++ WebApplication1/Models/AccessoriesModel.cs
@@ -5,5 +5,9 @@ namespace WebApplication1.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public decimal Price { get; set; }
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
     }
 }
